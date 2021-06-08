0d02cae7b81293067987268f3d5f6bf7e278a0db
Sanjay-Kurian <sanjayvkurian@gmail.com>
2021-06-08 12:51:45 +0530

    Added: Introducing functions to conditional Wages per month

--- empWage.sh
+++ empWage.sh
@@ -6,21 +6,25 @@ EMP_RATE_PER_HR=20
 NUM_WORKING_DAYS=20
 totalEmpHr=0
 totalWorkingDays=0
-while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+function getWorkingHours()
+{
+	 case $1 in
+      $IsFullTime)
+         workHours=8
+      ;;
+      $IsPartTime)
+         workHours=4
+      ;;
+      *)
+         workHours=0
+      ;;
+   esac
+	echo $workHours
+}
+while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
-	empCheck=$((RANDOM%3))
-	case $empCheck in
-		$IsFullTime)
-			empHrs=8
-		;;
-		$IsPartTime)
-			empHrs=4
-		;;
-		*)
-			empHrs=0
-		;;
-	esac
-	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+	workHours="$( getWorkingHours $((RANDOM%3)) )"
+	totalWorkHours=$(($totalWorkHours+$workHours))
 done
-totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+totalSalary=$(($totalWorkHours*$EMP_RATE_PER_HR))
