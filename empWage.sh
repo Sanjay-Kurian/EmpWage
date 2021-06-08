9fdd866ef87eca327f33744693de40538a1666e2
Sanjay-Kurian <sanjayvkurian@gmail.com>
2021-06-08 12:40:18 +0530

    Added: Conditional Wages per Month

--- empWage.sh
+++ empWage.sh
@@ -1,25 +1,26 @@
 #!/bin/bash -x
-fullTime=1
-partTime=2
-wagePerHour=20
-totalSalary=0
-numWorkingDays=20
-for((day=1;day<=$numWorkingDays;day++))
+IsFullTime=2
+IsPartTime=1
+MAX_HRS_IN_MONTH=10
+EMP_RATE_PER_HR=20
+NUM_WORKING_DAYS=20
+totalEmpHr=0
+totalWorkingDays=0
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
+	((totalWorkingDays++))
 	empCheck=$((RANDOM%3))
 	case $empCheck in
-		$fullTime)
+		$IsFullTime)
 			empHrs=8
 		;;
-		$partTime)
+		$IsPartTime)
 			empHrs=4
 		;;
 		*)
 			empHrs=0
 		;;
 	esac
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
 done
-salary=$(($empHrs*$wagePerHour))
-totalSalary=$(($totalSalary+$salary))
-
-
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
