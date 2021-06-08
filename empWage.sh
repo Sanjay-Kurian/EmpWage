85c8cfdca5acab0273f0b006c8e99ed8b1b09ad5
Sanjay-Kurian <sanjayvkurian@gmail.com>
2021-06-08 11:36:06 +0530

    Added: Daily Employee wage

--- empWage.sh
+++ empWage.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -x
 empCheck=$((RANDOM%2))
 isPresent=1
+wagePerHour=20
+fullDayHour=8
 if [ $empCheck -eq $isPresent ]
 then
 	echo Employee is present
+	daily=$(($wagePerHour*$fullDayHour))
+	echo "The daily wage of employee is" $daily
 else
 	echo Employee is absent
+	daily=0
+	echo "No daily wage"
 fi
 
