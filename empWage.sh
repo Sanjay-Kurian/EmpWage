7721b1bf96094e0e1cca1734bd2ea6ad3fa1e0eb
Sanjay-Kurian <sanjayvkurian@gmail.com>
2021-06-08 13:13:55 +0530

    Added:Storing daily wage in array

--- empWage.sh
+++ empWage.sh
@@ -4,7 +4,7 @@ IsPartTime=1
 MAX_HRS_IN_MONTH=10
 EMP_RATE_PER_HR=20
 NUM_WORKING_DAYS=20
-totalEmpHr=0
+totalWorkHours=0
 totalWorkingDays=0
 function getWorkingHours()
 {
@@ -21,10 +21,18 @@ function getWorkingHours()
    esac
 	echo $workHours
 }
+function calcDailyWages()
+{
+	workHrs=$1
+	wage=$(($workHrs*$EMP_RATE_PER_HR))
+	echo $wage
+}
 while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
 	workHours="$( getWorkingHours $((RANDOM%3)) )"
 	totalWorkHours=$(($totalWorkHours+$workHours))
+	empDailyWage[$totalWorkingDays]="$( calcDailyWages $workHours )"
 done
-totalSalary=$(($totalWorkHours*$EMP_RATE_PER_HR))
+totalSalary="$( calcDailyWages $totalWorkHours )"
+echo "Daily Wage " ${empDailyWage[@]}
