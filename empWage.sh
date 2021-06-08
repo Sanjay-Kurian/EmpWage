0cc9000605dd8149d659e06c862db3c17bacc7a0
Sanjay-Kurian <sanjayvkurian@gmail.com>
2021-06-08 12:07:54 +0530

    Added:Employee wage using Case Statements

--- empWage.sh
+++ empWage.sh
@@ -3,18 +3,18 @@ empCheck=$((RANDOM%3))
 fullTime=1
 partTime=2
 wagePerHour=20
-if [ $empCheck -eq $fullTime ]
-then
-	empHours=8
-	daily=$(($wagePerHour*$empHours))
-	echo "The daily wage of employee is" $daily
-elif [ $empCheck -eq $partTime ]
-then
-	empHours=8
-	daily=$(($wagePerHour*$empHours))
-	echo "The part time wage of employee is" $daily
-else
-	daily=0
-	echo "No daily wage"
-fi
+case $empCheck in
+	$fullTime)
+		empHrs=8
+	;;
+	$partTime)
+		empHrs=4
+	;;
+	*)
+		empHrs=0
+	;;
+esac
+salary=$(($empHrs*$wagePerHour))
+echo $salary
+
 
